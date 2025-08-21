3911fefcb15fc63d0b09d8c502f7de1b8de3f290
maksimkins <maks.aleshkov.04@gmail.com>
2025-08-21 21:33:28 +0400

    Add Blob to .Core

---
+++ Cut-Roll-News/src/Cut-Roll-News.Core/Common/Blob/Options/BlobOptions.cs
@@ -0,0 +1,6 @@
+namespace Cut_Roll_News.Core.Blob.BlobOptions;
+public class BlobOptions
+{
+    public string ContainerName { get; set; } = string.Empty;
+    public string Directory { get; set; } = string.Empty;
+}
